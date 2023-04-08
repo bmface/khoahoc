9f82bd1d47773221fdcd6ed7a8215d698e0747b8
CuongDev <cuongnm1794@gmail.com>
2023-04-08 17:03:01 +0700

    update ticket

--- admin/sql_noti.sql
+++
@@ -1,9 +0,0 @@
--- create table notification for user notification
-CREATE TABLE notifications (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    user_id INT(11) UNSIGNED NOT NULL,
-    message TEXT NOT NULL,
-    status BOOLEAN NOT NULL DEFAULT 0,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
